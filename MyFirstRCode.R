b9d37562977d846f5622bea305f54adcb006b620
unknown <shirfan805@gmail.com>
2022-10-17 21:06:53 +0600

    This is the very first commit for basic R with RStudio

---
+++ MyFirstRCode.R
@@ -0,0 +1,14 @@
+## The first code in any programming language
+print("Hello World! Hello Bangladesh!")
+
+## Set default language as English if error message shows in other language
+Sys.setenv(language = "en")
+
+## Install any package(say tidyverse)
+install.packages("tidyverse")
+
+## Install multiple packages with one code
+install.packages(c("nycflights13", "gapminder", "Lahman"))
+
+## Get the updated package
+tidyverse_update()
